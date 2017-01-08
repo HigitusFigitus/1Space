029e46682dffbc0d5234f566bf33c4a2a5cdeb8b
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-01-08 15:48:12 -0800

    Adds jQuery to handle ajax request

---
+++ app/assets/javascripts/texts.js
@@ -0,0 +1,15 @@
+$(document).on("turbolinks:load", function(){
+  submitTextFormListener();
+});
+
+var submitTextFormListener = function(){
+  $("#text-form").on("ajax:success", function(event, response){
+    console.log("Ajax successful");
+
+    $("#text-form").html('<strong>' + '<mark>' + response.extra_spaces + '</mark>' + " extra spaces were removed from your text: " + '</strong>' + "<br>" + response.corrected_text).addClass("card text-justify text-success").css({backgroundColor: "#eeeeee", borderRadius: "5px", padding: "20px" }).after('<p>' +'<a href="/" class="btn btn-primary btn-lg btn-block">Submit new text</a>' + '</p>');
+  })
+
+  $("#text-form").on("ajax:error", function(event, response){
+    console.log("Ajax unsuccessful");
+  })
+}
