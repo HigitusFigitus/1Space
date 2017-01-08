758e31ab7b8ff02af19581d9bb8b75360c6ff673
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-01-08 15:49:33 -0800

    Adds api_response class with test data

---
+++ app/models/api_response.rb
@@ -0,0 +1,19 @@
+class ApiResponse
+  include ActiveModel::Model
+
+  def self.create_corrected_text(inputted_text)
+    "#{inputted_text} has been received."
+    # text_from_page.gsub(/\. +/, ". ")
+  end
+
+  def self.record_changes(inputted_text)
+    6
+  end
+
+  def self.build_response(inputted_text)
+    {
+      corrected_text: create_corrected_text(inputted_text),
+      extra_spaces: record_changes(inputted_text)
+    }.to_json
+  end
+end
