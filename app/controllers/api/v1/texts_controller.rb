d7094f89002e47718e4cb44c3b940d85e91b9ffc
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-01-08 15:56:23 -0800

    Removes debugging statements

--- app/controllers/api/v1/texts_controller.rb
+++ app/controllers/api/v1/texts_controller.rb
@@ -2,9 +2,6 @@ class Api::V1::TextsController < ApplicationController
   def index
     @text = ApiResponse.build_response(params[:text])
 
-    puts params[:text]
-    puts @text
-
     respond_to do |format|
       format.js
       render json: @text
