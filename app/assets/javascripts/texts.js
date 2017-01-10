eac03223a90f94ec4e540c0448cf26c09383feb2
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-01-09 17:29:10 -0800

    Refactors ajax response for better message to user

--- app/assets/javascripts/texts.js
+++ app/assets/javascripts/texts.js
@@ -6,7 +6,7 @@ var submitTextFormListener = function(){
   $("#text-form").on("ajax:success", function(event, response){
     console.log("Ajax successful");
 
-    $("#text-form").html('<strong>' + '<mark>' + response.extra_spaces + '</mark>' + " extra spaces were removed from your text: " + '</strong>' + "<br>" + response.corrected_text).addClass("card text-justify text-success").css({backgroundColor: "#eeeeee", borderRadius: "5px", padding: "20px" }).after('<p>' +'<a href="/" class="btn btn-primary btn-lg btn-block">Submit new text</a>' + '</p>');
+    $("#text-form").html('<strong>' + response.extra_spaces + '</strong>' + "<br>" + response.corrected_text).addClass("card text-justify text-success").css({backgroundColor: "#eeeeee", borderRadius: "5px", padding: "20px" }).after('<p>' +'<a href="/" class="btn btn-primary btn-lg btn-block">Submit new text</a>' + '</p>');
   })
 
   $("#text-form").on("ajax:error", function(event, response){
